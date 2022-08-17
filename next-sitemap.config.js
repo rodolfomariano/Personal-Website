a9cffc8ee54ce59701808fcab77de03ed0e7f3a3
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2022-08-17 11:33:21 -0300

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://rodolfo.dev.br',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
