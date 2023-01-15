fee25906ac8f0ac210e8820c36bb6317db8eb10c
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2023-01-15 13:49:39 -0300

    edit curriculum url

--- src/components/UserImage/index.tsx
+++ src/components/UserImage/index.tsx
@@ -13,7 +13,7 @@ import { ProfileImg } from '../ProfileImg'
 
 export function UserImage() {
   const urlToDownload =
-    'https://firebasestorage.googleapis.com/v0/b/imagens-3dce0.appspot.com/o/Curriculum%20Rodolfo%20(1).pdf?alt=media&token=266e2e54-3609-40ed-966d-c2406543f78e'
+    'https://firebasestorage.googleapis.com/v0/b/imagens-3dce0.appspot.com/o/Rodolfo%20Curriculum%202023.pdf?alt=media&token=b48b9a89-49d2-4312-8680-7d3af568c372'
 
   return (
     <Container
